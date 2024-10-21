261464fbc37334d0a46c8ad679ae6b3fd9ea690d
Antony-M1 <antony.praveenkumar@softsuave.com>
2024-10-21 19:29:02 +0530

    Flake8 W291

--- ats_llm.py
+++ ats_llm.py
@@ -43,8 +43,8 @@ input_prompt = """
 Hey Act Like a skilled or very experience ATS(Application Tracking System)
 with a deep understanding of tech field,software engineering,data science ,data analyst
 and big data engineer. Your task is to evaluate the resume based on the given job description.
-You must consider the job market is very competitive and you should provide 
-best assistance for improving the resumes. Assign the percentage Matching based 
+You must consider the job market is very competitive and you should provide
+best assistance for improving the resumes. Assign the percentage Matching based
 on Jd and
 the missing keywords with high accuracy
 resume:{text}
